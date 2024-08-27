7d09ba1486a6722d871d6c0b42584774e82205d7
Mike Walmsley <walmsleymk1@gmail.com>
2024-08-27 15:31:22 -0400

    download

--- download/download_hf_dataset_from_hub.py
+++ download/download_hf_dataset_from_hub.py
@@ -4,6 +4,8 @@ from torch.utils.data import DataLoader
 
 if __name__ == '__main__':
 
-    ds = load_dataset("mwalmsley/gz_evo", split="train", name='tiny')
+    # ds = load_dataset("mwalmsley/gz_evo", split="train", name='tiny')
+    # ds = load_dataset("mwalmsley/gz_hubble", name='tiny')
+    ds = load_dataset("mwalmsley/gz_hubble", name='default')
     ds.set_format("torch")  # also supports numpy, jax, etc
-    dataloader = DataLoader(ds, batch_size=4, num_workers=1)
+    dataloader = DataLoader(ds['train'], batch_size=4, num_workers=1)
