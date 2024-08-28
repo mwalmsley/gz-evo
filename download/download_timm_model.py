fc9b191704d76907b67a629e1ab3f9d70fa17c6d
Mike Walmsley <walmsleymk1@gmail.com>
2024-08-28 09:23:22 -0400

    more models

--- download/download_timm_model.py
+++ download/download_timm_model.py
@@ -11,7 +11,31 @@ if __name__ == '__main__':
     for model in [
         'resnet18',
         'resnet50',
+        'resnet101',
+
+        'convnext_pico', 
         'convnext_nano',
+        'convnext_tiny',
+        'convnext_small', 
         'convnext_base',
+        'convnext_large',
+        # 'convnext_xlarge',
+
+        'convnextv2_nano.fcmae',
+        'convnextv2_nano.fcmae_ft_in22k_in1k',
+        'convnextv2_base.fcmae_ft_in22k_in1k',
+        'convnext_base.clip_laion2b_augreg_ft_in12k_in1',
+
+        'efficientnet_b0',
+        'tf_efficientnetv2_s',
+        'tf_efficientnetv2_l',
+        # 'tf_efficientnetv2_xl'
+
+        'maxvit_tiny_rw_224',
+        'maxvit_rmlp_small_rw_224', 
+        'maxvit_rmlp_base_rw_224',
+        'maxvit_large_tf_224',
+        # 'maxvit_xlarge_tf_224'
+
     ]:
         model = timm.create_model(model, pretrained=True)
