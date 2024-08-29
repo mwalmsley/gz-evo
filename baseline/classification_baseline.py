c6b7a88e9b2cb715242590a3035aaf12812e1475
Mike Walmsley <walmsleymk1@gmail.com>
2024-08-29 13:47:17 -0400

    run pico without stopping early

--- baseline/classification_baseline.py
+++ baseline/classification_baseline.py
@@ -11,9 +11,10 @@ import baseline_training  # relative import
 
 def main():
     # architecture_name = 'resnet50'
-    # architecture_name = 'convnext_pico'
+    architecture_name = 'convnext_pico'
     # architecture_name = 'convnext_atto'
-    architecture_name = 'convnext_base'
+    # architecture_name = 'convnext_nano'
+    # architecture_name = 'convnext_base'
 
     # dataset_name='gz_evo'
     # dataset_name='gz_hubble'
