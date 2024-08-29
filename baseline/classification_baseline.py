56c403c3c78fa490ad37cbe0746bbead2d9ba3e8
Mike Walmsley <walmsleymk1@gmail.com>
2024-08-29 16:02:45 -0400

    and for nano as well

--- baseline/classification_baseline.py
+++ baseline/classification_baseline.py
@@ -11,9 +11,9 @@ import baseline_training  # relative import
 
 def main():
     # architecture_name = 'resnet50'
-    architecture_name = 'convnext_pico'
+    # architecture_name = 'convnext_pico'
     # architecture_name = 'convnext_atto'
-    # architecture_name = 'convnext_nano'
+    architecture_name = 'convnext_nano'
     # architecture_name = 'convnext_base'
 
     # dataset_name='gz_evo'
