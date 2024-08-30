961299111aeb89a2ee1ea25d506c4cb15ce2fdb7
Mike Walmsley <walmsleymk1@gmail.com>
2024-08-29 22:27:07 -0400

    hopeful on nano

--- baseline/classification_baseline.py
+++ baseline/classification_baseline.py
@@ -16,8 +16,8 @@ def main():
     # architecture_name = 'resnet50'
     # architecture_name = 'convnext_pico'
     # architecture_name = 'convnext_atto'
-    # architecture_name = 'convnext_nano'
-    architecture_name = 'convnext_base'
+    architecture_name = 'convnext_nano'
+    # architecture_name = 'convnext_base'
     # base evo now started as 7222, long filtering step, others waiting for this
     # filtering completed but epoch is pretty long, 1 hour
     # starting another with atto for quick check, 7263
