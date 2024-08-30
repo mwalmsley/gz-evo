de14e9dcb8df0b06df9fe1eba5912b58a88a513b
Mike Walmsley <walmsleymk1@gmail.com>
2024-08-30 11:19:48 -0400

    maxvit

--- baseline/classification_baseline.py
+++ baseline/classification_baseline.py
@@ -22,7 +22,8 @@ def main():
     # architecture_name = 'convnext_base'
     # architecture_name = 'resnet50_clip.openai'
     # architecture_name = 'efficientnet_b0'
-    architecture_name = 'tf_efficientnetv2_s'
+    # architecture_name = 'tf_efficientnetv2_s'
+    architecture_name = 'maxvit_tiny_rw_224'
     # base evo now started as 7222, long filtering step, others waiting for this
     # filtering completed but epoch is pretty long, 1 hour
     # starting another with atto for quick check, 7263
