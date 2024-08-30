e7d12280b583a8e9156f585578800ec24008ec56
Mike Walmsley <walmsleymk1@gmail.com>
2024-08-30 11:18:17 -0400

    b0

--- baseline/classification_baseline.py
+++ baseline/classification_baseline.py
@@ -20,7 +20,8 @@ def main():
     # architecture_name = 'convnext_atto'
     # architecture_name = 'convnext_nano'
     # architecture_name = 'convnext_base'
-    architecture_name = 'resnet50_clip.openai'
+    # architecture_name = 'resnet50_clip.openai'
+    architecture_name = 'efficientnet_b0'
     # base evo now started as 7222, long filtering step, others waiting for this
     # filtering completed but epoch is pretty long, 1 hour
     # starting another with atto for quick check, 7263
