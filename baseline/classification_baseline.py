3edb59cdf8fc4091275002553c5de49b210774f3
Mike Walmsley <walmsleymk1@gmail.com>
2024-08-30 11:19:22 -0400

    v2

--- baseline/classification_baseline.py
+++ baseline/classification_baseline.py
@@ -21,7 +21,8 @@ def main():
     # architecture_name = 'convnext_nano'
     # architecture_name = 'convnext_base'
     # architecture_name = 'resnet50_clip.openai'
-    architecture_name = 'efficientnet_b0'
+    # architecture_name = 'efficientnet_b0'
+    architecture_name = 'tf_efficientnetv2_s'
     # base evo now started as 7222, long filtering step, others waiting for this
     # filtering completed but epoch is pretty long, 1 hour
     # starting another with atto for quick check, 7263
