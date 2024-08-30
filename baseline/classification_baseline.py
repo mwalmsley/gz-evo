f51f680ac8c911171a7491a86bad39cb7fd65360
Mike Walmsley <walmsleymk1@gmail.com>
2024-08-30 13:28:21 -0400

    resnet again

--- baseline/classification_baseline.py
+++ baseline/classification_baseline.py
@@ -15,7 +15,7 @@ def main():
     # these are all good to be run on gz evo on galahad (although filter takes ages)
     # do not raise the learning rate, it seems to break training (strangely)
 
-    # architecture_name = 'resnet50'
+    architecture_name = 'resnet50'
     # architecture_name = 'convnext_pico'
     # architecture_name = 'convnext_atto'
     # architecture_name = 'convnext_nano'
@@ -23,7 +23,7 @@ def main():
     # architecture_name = 'resnet50_clip.openai'
     # architecture_name = 'efficientnet_b0'
     # architecture_name = 'tf_efficientnetv2_s'
-    architecture_name = 'maxvit_tiny_rw_224'
+    # architecture_name = 'maxvit_tiny_rw_224'
     # base evo now started as 7222, long filtering step, others waiting for this
     # filtering completed but epoch is pretty long, 1 hour
     # starting another with atto for quick check, 7263
