82428fd4b786f2c1adb968dbfede020af798a38b
Mike Walmsley <walmsleymk1@gmail.com>
2024-08-30 11:14:35 -0400

    base beluga

--- baseline/classification_baseline.py
+++ baseline/classification_baseline.py
@@ -16,10 +16,10 @@ def main():
     # do not raise the learning rate, it seems to break training (strangely)
 
     # architecture_name = 'resnet50'
-    architecture_name = 'convnext_pico'
+    # architecture_name = 'convnext_pico'
     # architecture_name = 'convnext_atto'
     # architecture_name = 'convnext_nano'
-    # architecture_name = 'convnext_base'
+    architecture_name = 'convnext_base'
     # base evo now started as 7222, long filtering step, others waiting for this
     # filtering completed but epoch is pretty long, 1 hour
     # starting another with atto for quick check, 7263
