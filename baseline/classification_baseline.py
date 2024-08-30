ac120e91134b144343101614731918df70691bbb
Mike Walmsley <walmsleymk1@gmail.com>
2024-08-30 14:22:10 -0400

    maxvit

--- baseline/classification_baseline.py
+++ baseline/classification_baseline.py
@@ -20,10 +20,10 @@ def main():
     # architecture_name = 'convnext_atto'
     # architecture_name = 'convnext_nano'
     # architecture_name = 'convnext_base'
-    architecture_name = 'resnet50_clip.openai'
+    # architecture_name = 'resnet50_clip.openai'
     # architecture_name = 'efficientnet_b0'
     # architecture_name = 'tf_efficientnetv2_s'
-    # architecture_name = 'maxvit_tiny_rw_224'
+    architecture_name = 'maxvit_tiny_rw_224'
     # architecture_name = 'convnextv2_base.fcmae_ft_in22k_in1k'
     # architecture_name = 'convnext_base.clip_laion2b_augreg_ft_in12k'
 
