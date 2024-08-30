65ec478095e9bf49651c9f64334ac36d4d275b03
Mike Walmsley <walmsleymk1@gmail.com>
2024-08-30 05:07:41 -0400

    snapshot download

--- download/download_hf_dataset_from_hub.py
+++ download/download_hf_dataset_from_hub.py
@@ -14,14 +14,17 @@ if __name__ == '__main__':
         os.environ['HF_HOME']="/share/nas2/walml/cache/huggingface"
         os.environ['HF_DATASETS_CACHE']="/share/nas2/walml/cache/huggingface/datasets"
 
-    from datasets import load_dataset
+    from datasets import load_dataset, snapshot_download
     from torch.utils.data import DataLoader
 
     # ds = load_dataset("mwalmsley/gz_evo", split="train", name='tiny')
     # ds = load_dataset("mwalmsley/gz_hubble", name='tiny')
     # ds = load_dataset("mwalmsley/gz_hubble", name='default')
     # ds = load_dataset("mwalmsley/gz2", name='default')
-    load_dataset("mwalmsley/gz_evo", name='default', split='test')
-    load_dataset("mwalmsley/gz_evo", name='default', split='train')
+    # load_dataset("mwalmsley/gz_evo", name='default', split='test')
+    # load_dataset("mwalmsley/gz_evo", name='default', split='train')
     # ds.set_format("torch")  # also supports numpy, jax, etc
     # dataloader = DataLoader(ds['train'], batch_size=4, num_workers=1)
+    
+    # s all downloaded files are also cached on your local disk
+    snapshot_download(repo_id="mwalmsley/gz_evo", repo_type="dataset")
