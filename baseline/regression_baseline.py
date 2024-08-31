ba8982f6fd2a32b776c8f916c36c720149d40d53
Mike Walmsley <walmsleymk1@gmail.com>
2024-08-31 13:55:20 -0400

    run all regression preds

--- baseline/regression_baseline.py
+++ baseline/regression_baseline.py
@@ -64,12 +64,13 @@ def evaluate():
 
     for dataset_name, architecture_name, checkpoint_dir in [
         #  ('gz2', 'convnext_pico', debug_dir + 'convnext_pico_534895718')
-        # ('gz_evo', 'convnext_atto', beluga_dir + 'convnext_atto_534895718'),
+        # ('gz_evo', 'convnext_atto', beluga_dir + 'convnext_atto_534895718'),  # old, doesn't load properly
         # ('gz_evo', 'convnext_pico',  beluga_dir + 'convnext_pico_534895718'),
-        # ('gz_evo', 'convnext_nano',  beluga_dir + 'convnext_nano_534895718'),
-        ('gz_evo', 'convnext_base',  beluga_dir + 'convnext_base_534895718'),
-        # ('gz_evo', 'maxvit_tiny_rw_224',  beluga_dir + 'maxvit_tiny_rw_224_534895718'),
-        # ('gz_evo', 'tf_efficientnetv2_s',  beluga_dir + 'tf_efficientnetv2_s_534895718')
+        ('gz_evo', 'convnext_nano',  beluga_dir + 'convnext_nano_534895718'),
+        # ('gz_evo', 'convnext_base',  beluga_dir + 'convnext_base_534895718'),
+        ('gz_evo', 'maxvit_tiny_rw_224',  beluga_dir + 'maxvit_tiny_rw_224_534895718'),
+        ('gz_evo', 'tf_efficientnetv2_s',  beluga_dir + 'tf_efficientnetv2_s_534895718'),
+        ('gz_evo', 'convnext_base.clip_laion2b_augreg_ft_in12k', beluga_dir + 'convnext_base.clip_laion2b_augreg_ft_in12k_534895718')
     ]:
 
         logging.info(f"Evaluating {dataset_name} {architecture_name} {checkpoint_dir}")
--- slurm_examples/beluga.sh
+++ slurm_examples/beluga.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=01:30:00  
+#SBATCH --time=12:30:00  
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
