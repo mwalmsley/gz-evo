bdcbc270ff27f7c48208b978e110336f0a7aec05
Mike Walmsley <walmsleymk1@gmail.com>
2024-08-30 21:44:58 -0400

    resnet50 evo

--- baseline/regression_baseline.py
+++ baseline/regression_baseline.py
@@ -23,11 +23,11 @@ def main():
     # horribly broken
     # now adjusted loss
 
-    # architecture_name = 'resnet50'
+    architecture_name = 'resnet50'
     # architecture_name = 'convnext_nano'
     # architecture_name = 'convnext_pico'
     # architecture_name = 'convnext_atto'
-    architecture_name = 'convnext_base'
+    # architecture_name = 'convnext_base'
 
     # architecture_name = 'tf_efficientnetv2_s'
     # architecture_name = 'maxvit_tiny_rw_224'
