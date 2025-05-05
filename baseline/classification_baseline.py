169713f72411896a3172fa566bfe17fbbe56d76f
Mike Walmsley <walmsleymk1@gmail.com>
2025-05-05 16:54:47 -0400

    resnet50

--- baseline/classification_baseline.py
+++ baseline/classification_baseline.py
@@ -15,14 +15,14 @@ def main():
     # these are all good to be run on gz evo on galahad (although filter takes ages)
     # do not raise the learning rate, it seems to break training (strangely)
 
-    # architecture_name = 'resnet50'
+    architecture_name = 'resnet50'
     # architecture_name = 'resnet50_clip.openai'
 
     # architecture_name = 'convnext_atto'
     # architecture_name = 'convnext_pico'
     # architecture_name = 'convnext_nano'
     # architecture_name = 'convnext_base'
-    architecture_name = 'convnextv2_base.fcmae_ft_in22k_in1k'
+    # architecture_name = 'convnextv2_base.fcmae_ft_in22k_in1k'
     # architecture_name = 'convnext_base.clip_laion2b_augreg_ft_in12k'
 
     # architecture_name = 'efficientnet_b0'
