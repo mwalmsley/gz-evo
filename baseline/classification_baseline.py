0a458099600c439d2877f0d41d02a25150eca08a
Mike Walmsley <walmsleymk1@gmail.com>
2025-05-06 10:00:13 -0400

    maxvit base

--- baseline/classification_baseline.py
+++ baseline/classification_baseline.py
@@ -30,8 +30,8 @@ def main():
     # architecture_name = 'tf_efficientnetv2_s'
 
     # architecture_name = 'maxvit_tiny'
-    architecture_name = 'maxvit_small'
-    # architecture_name = 'maxvit_base'
+    # architecture_name = 'maxvit_small'
+    architecture_name = 'maxvit_base'
     # architecture_name = 'maxvit_large'
 
     # architecture_name = 'convnext_nano_finetune'
