e5b2a5f95ac108d3daa37c7cb7d84c987e1cb8e5
Mike Walmsley <walmsleymk1@gmail.com>
2025-05-07 15:16:34 -0400

    typo

--- baseline/regression_baseline.py
+++ baseline/regression_baseline.py
@@ -76,11 +76,11 @@ def evaluate():
     results_dir = '/share/nas2/walml/repos/gz-evo/results/baselines/regression/'
 
     for dataset_name, architecture_name, checkpoint_dir in [
-        ('gz_evo', 'resnet50',  results_dir + 'resnet50_534895718_1746547649')
+        ('gz_evo', 'resnet50',  results_dir + 'resnet50_534895718_1746547649'),
         ('gz_evo', 'convnext_nano',  results_dir + 'convnext_nano_534895718_1746542691'),
         ('gz_evo', 'convnext_base',  results_dir + 'convnext_base_534895718_1746547550'),
         # ('gz_evo', 'maxvit_tiny_rw_224',  results_dir + 'maxvit_tiny_rw_224_534895718_1746547757'), WIP
-        ('gz_evo', 'tf_efficientnetv2_s',  results_dir + 'tf_efficientnetv2_s_534895718_1746547782'),
+        ('gz_evo', 'tf_efficientnetv2_s',  results_dir + 'tf_efficientnetv2_s_534895718_1746547782')
     ]:
 
         logging.info(f"Evaluating {dataset_name} {architecture_name} {checkpoint_dir}")
