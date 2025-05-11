4761397f3b284be4a7a415f7e2e3ee6e6f19d416
Mike Walmsley <walmsleymk1@gmail.com>
2025-05-11 11:44:32 -0400

    switch mode

--- slurm_examples/galahad.sh
+++ slurm_examples/galahad.sh
@@ -29,11 +29,11 @@ export GZ_EVO_MANUAL_DOWNLOAD_LOC='/share/nas2/walml/tmp/gz-evo'
 PYTHON="/share/nas2/walml/miniconda3/envs/zoobot39_cu118_dev/bin/python"
 REPO_DIR="/share/nas2/walml/repos/gz-evo"
 
-# echo 'Running classification baseline'
-# $PYTHON $REPO_DIR/baseline/classification_baseline.py 
+echo 'Running classification baseline'
+$PYTHON $REPO_DIR/baseline/classification_baseline.py 
 
-echo 'Running regression baseline'
-$PYTHON $REPO_DIR/baseline/regression_baseline.py 
+# echo 'Running regression baseline'
+# $PYTHON $REPO_DIR/baseline/regression_baseline.py 
 
 # publish to hub
 # echo 'Publishing encoders to hub'
