7fb9793393f32c3eb19ce5617fe9de3146d5486f
Mike Walmsley <walmsleymk1@gmail.com>
2025-05-11 11:43:19 -0400

    final classification eval

--- baseline/classification_baseline.py
+++ baseline/classification_baseline.py
@@ -146,9 +146,17 @@ def evaluate():
         # ('gz_evo', 'convnext_base',  results_dir + 'convnext_base_finetune_494155588'),
         # ('gz_evo', 'convnext_nano',  results_dir + 'convnext_nano_534895718'),  # not great, might need redo. Redone and still not great.
         # ('gz_evo', 'convnext_nano',  results_dir + 'convnext_nano_finetune_494155588'),  # failed
-        ('gz_evo', 'maxvit_tiny',  results_dir + 'maxvit_tiny_rw_224_534895718'),
-        ('gz_evo', 'resnet50',  results_dir + 'resnet50_534895718'),
-        ('gz_evo', 'convnextv2_base.fcmae_ft_in22k_in1k',  results_dir + 'convnextv2_base.fcmae_ft_in22k_in1k_534895718'), 
+        
+        # ('gz_evo', 'maxvit_tiny',  results_dir + 'maxvit_tiny_rw_224_534895718'),
+        # ('gz_evo', 'resnet50',  results_dir + 'resnet50_534895718'),
+        # ('gz_evo', 'convnextv2_base.fcmae_ft_in22k_in1k',  results_dir + 'convnextv2_base.fcmae_ft_in22k_in1k_534895718'),
+        
+        ('gz_evo', 'convnext_large', results_dir + 'convnext_large_534895718'),
+        ('gz_evo', 'maxvit_large', results_dir + 'maxvit_large_534895718'),
+        ('gz_evo', 'maxvit_small', results_dir + 'maxvit_small_3966912'),
+        ('gz_evo', 'tf_efficientnetv2_l', results_dir + 'tf_efficientnetv2_l_534895718'),
+        ('gz_evo', 'tf_efficientnetv2_m', results_dir + 'tf_efficientnetv2_m_534895718'),
+
     ]:
         logging.info(f"Evaluating {dataset_name} {architecture_name} {checkpoint_dir}")
         cfg = baseline_training.get_config(architecture_name, dataset_name, save_dir='foobar') # save_dir is not used
@@ -207,5 +215,5 @@ if __name__ == "__main__":
     # seed = 43  # for testing the new 'core finetuning' idea
     pl.seed_everything(seed)
 
-    main()
-    # evaluate()
+    # main()
+    evaluate()
