eaae606fe2567c3fc8cc4f08f2ec3b89b7ceaf45
Mike Walmsley <walmsleymk1@gmail.com>
2025-05-29 12:06:02 -0400

    retry distributing dataset

--- gz_evo/core/baseline_datamodules.py
+++ gz_evo/core/baseline_datamodules.py
@@ -30,6 +30,20 @@ def add_validation_split(dataset_dict, seed=42, num_workers=4):
     del train_and_val_dict
     return dataset_dict
 
+def distribute_dataset_with_lightning(dataset_dict):
+    rank = int(os.environ.get("SLURM_PROCID", 0))  # index of slurm task
+    world_size = int(os.environ.get("NTASKS_PER_NODE", 1))  # number of slurm tasks = world size for single node
+
+    logging.info('Beginning data loading on rank {}, world size {}'.format(rank, world_size))
+
+    if (rank > 0) or (world_size > 1):
+        logging.info(f"Distributing datasets on rank {rank}, world size {world_size}")
+        dataset_dict  = split_dataset_by_node(dataset_dict, rank=rank, world_size=world_size)
+    else:
+        logging.info(f"Not distributing datasets on rank {rank}, world {world_size}, single gpu training")
+
+    return dataset_dict
+
 # https://pytorch-lightning.readthedocs.io/en/stable/extensions/datamodules.html
 class GenericDataModule(pl.LightningDataModule):
     def __init__(
@@ -124,18 +138,15 @@ class GenericDataModule(pl.LightningDataModule):
         if stage == "fit" or stage is None:
 
             # distributed training
-            rank = int(os.environ.get("LOCAL_RANK", 0))  # local rank for this process
+            # TODO may move to train script for efficiency
+            self.dataset_dict = distribute_dataset_with_lightning(self.dataset_dict)
+
+            # with lightning, these aren't set as you'd expect, remain unset
+            # rank = int(os.environ.get("LOCAL_RANK", 0))  # local rank for this process
             # world_size = int(os.environ.get("WORLD_SIZE", 1))  # total number of processes
-            world_size = int(os.environ.get("NTASKS_PER_NODE", 1))  # for single node, this is world size
-            slurm_procid = int(os.environ.get("SLURM_PROCID", 0))  # for slurm, this is the process id
 
-            logging.info('Beginning data loading on rank {}, slurm_procid {}, world size {}'.format(rank, slurm_procid, world_size))
-
-            if (rank > 0) or (world_size > 1):
-                logging.info(f"Distributing datasets on rank {rank}, world size {world_size}")
-                self.dataset_dict  = split_dataset_by_node(self.dataset_dict, rank=rank, world_size=world_size)
-            else:
-                logging.info(f"Not distributing datasets on rank {rank}, world {world_size}, single gpu training")
+            # use env variables from slurm instead
+            distribute_dataset_with_lightning()
 
 
             if self.iterable:
