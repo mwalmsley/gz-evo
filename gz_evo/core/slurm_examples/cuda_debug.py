bc2bce4e03920769b2a6afae56bba5d04c684855
Mike Walmsley <walmsleymk1@gmail.com>
2025-06-02 13:50:38 -0400

    add

--- gz_evo/core/slurm_examples/cuda_debug.py
+++ gz_evo/core/slurm_examples/cuda_debug.py
@@ -1,17 +1,22 @@
 import time
+import logging
 
 import torch
 import pytorch_lightning as pl
 
 
 if __name__ == "__main__":
-    print("Running CUDA debug script...")
 
-    print(torch.cuda.is_available())
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-    print(torch.cuda.device_count())
 
-    print(torch.cuda.current_device())
+    logging.info("Running CUDA debug script...")
+
+    logging.info(torch.cuda.is_available())
+
+    logging.info(torch.cuda.device_count())
+
+    logging.info(torch.cuda.current_device())
 
     from lightning.pytorch.accelerators import find_usable_cuda_devices
 
@@ -20,7 +25,7 @@ if __name__ == "__main__":
         def __init__(self):
             super().__init__()
             self.layer = torch.nn.Linear(10, 1)
-            print("Dummy model initialized on device:", self.layer.weight.device)
+            logging.info("Dummy model initialized on device:", self.layer.weight.device)
 
         def forward(self, x):
             time.sleep(1)
@@ -47,30 +52,30 @@ if __name__ == "__main__":
     # this is not very useful - only checks if a tensor can be placed
     # not if you can put the whole model on the device
     # devices = find_usable_cuda_devices(1)
-    # print("Usable CUDA devices:", devices)
+    # logging.info("Usable CUDA devices:", devices)
 
     device_mem = {}
     for device in range(torch.cuda.device_count()):
-        print(f"Device {device}: {torch.cuda.get_device_name(device)}")
-        print(f"  Memory Allocated: {torch.cuda.memory_allocated(device)} bytes")
-        print(f"  Memory Cached: {torch.cuda.memory_reserved(device)} bytes")
+        logging.info(f"Device {device}: {torch.cuda.get_device_name(device)}")
+        logging.info(f"  Memory Allocated: {torch.cuda.memory_allocated(device)} bytes")
+        logging.info(f"  Memory Cached: {torch.cuda.memory_reserved(device)} bytes")
         device_mem[device] = {
             'allocated': torch.cuda.memory_allocated(device),
             'cached': torch.cuda.memory_reserved(device)
         }
 
     lowest_mem_device = min(device_mem, key=lambda k: device_mem[k]['allocated'])
-    print(f"Lowest memory device: {lowest_mem_device} with {device_mem[lowest_mem_device]['allocated']} bytes allocated")
+    logging.info(f"Lowest memory device: {lowest_mem_device} with {device_mem[lowest_mem_device]['allocated']} bytes allocated")
     devices = [lowest_mem_device]  # use the device with the lowest memory allocation
-    print("Using devices:", devices)
+    logging.info("Using devices:", devices)
 
-    x = torch.randn(10000, 10).to(f'cuda:{lowest_mem_device}')
-    print("Tensor created on device:", x.device)
-    print("new memory: ", torch.cuda.memory_allocated(lowest_mem_device))
+    x = torch.randn(100000, 10).to(f'cuda:{lowest_mem_device}')
+    logging.info("Tensor created on device:", x.device)
+    logging.info("new memory: ", torch.cuda.memory_allocated(lowest_mem_device))
 
-    time.sleep(1000)
+    # time.sleep(1000)
 
-    print('Releasing memory...')
+    logging.info('Releasing memory...')
     
 
     # trainer = pl.Trainer(
@@ -83,4 +88,4 @@ if __name__ == "__main__":
     #     model=DummyModel(),
     #     datamodule=DummyDataModule()
     # )
-    # print("Training completed.")
+    # logging.info("Training completed.")
