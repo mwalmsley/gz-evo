324cb9e4ccdb25600dcf6f1ec52b7b66ec5fb33e
Mike Walmsley <walmsleymk1@gmail.com>
2025-06-02 13:33:25 -0400

    pick lowest mem device manually

--- gz_evo/core/slurm_examples/cuda_debug.py
+++ gz_evo/core/slurm_examples/cuda_debug.py
@@ -44,8 +44,25 @@ if __name__ == "__main__":
 
 
 
-    devices = find_usable_cuda_devices(1)
-    print("Usable CUDA devices:", devices)
+    # this is not very useful - only checks if a tensor can be placed
+    # not if you can put the whole model on the device
+    # devices = find_usable_cuda_devices(1)
+    # print("Usable CUDA devices:", devices)
+
+    device_mem = {}
+    for device in range(torch.cuda.device_count()):
+        print(f"Device {device}: {torch.cuda.get_device_name(device)}")
+        print(f"  Memory Allocated: {torch.cuda.memory_allocated(device)} bytes")
+        print(f"  Memory Cached: {torch.cuda.memory_reserved(device)} bytes")
+        device_mem[device] = {
+            'allocated': torch.cuda.memory_allocated(device),
+            'cached': torch.cuda.memory_reserved(device)
+        }
+
+    lowest_mem_device = min(device_mem, key=lambda k: device_mem[k]['allocated'])
+    print(f"Lowest memory device: {lowest_mem_device} with {device_mem[lowest_mem_device]['allocated']} bytes allocated")
+    devices = [lowest_mem_device]  # use the device with the lowest memory allocation
+    print("Using devices:", devices)
 
     trainer = pl.Trainer(
         devices=devices,
