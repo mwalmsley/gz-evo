fe2df9767409aed038fc56847a47e513cfd48a48
Mike Walmsley <walmsleymk1@gmail.com>
2025-06-02 13:03:07 -0400

    use lightning utility instead since squeue flags fail

--- gz_evo/core/slurm_examples/cuda_debug.py
+++ gz_evo/core/slurm_examples/cuda_debug.py
@@ -1,4 +1,8 @@
+import time
+
 import torch
+import pytorch_lightning as pl
+
 
 if __name__ == "__main__":
     print("Running CUDA debug script...")
@@ -8,3 +12,15 @@ if __name__ == "__main__":
     print(torch.cuda.device_count())
 
     print(torch.cuda.current_device())
+
+    from lightning.pytorch.accelerators import find_usable_cuda_devices
+
+    trainer = pl.Trainer(
+        devices=find_usable_cuda_devices(1),
+        accelerator='cuda',
+        max_epochs=1
+    )
+    print("Trainer created with usable CUDA devices.")
+    print(trainer.devices)
+
+    time.sleep(60)  # Sleep to allow time to check the output
