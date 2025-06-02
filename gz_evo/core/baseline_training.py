bd8945751c57c0cd238d1d8f3569bf3944e7f5bf
Mike Walmsley <walmsleymk1@gmail.com>
2025-06-02 11:16:54 -0400

    maybe unlucky timeout?

--- gz_evo/core/baseline_training.py
+++ gz_evo/core/baseline_training.py
@@ -36,11 +36,11 @@ def get_config(architecture_name, dataset_name, save_dir, debug=False):
     elif os.path.isdir('/share/nas2'):
         subset_name = 'default'
         # subset_name = 'tiny' 
-        num_workers = 12  # of 24 per node on some, or 16 on others
+        num_workers = 10  # of 24 per node on some, or 16 on others
         batch_size_key = 'a100_batch_size'
         accelerator="gpu"
         devices = int(os.environ.get('SLURM_NTASKS_PER_NODE', 1))
-        prefetch_factor = 10
+        prefetch_factor = 8
 
     elif os.path.isdir('/Users/user'):
         # macbook
