5fc1aaa96c28556494492c42677ddf2cbfd16d18
Mike Walmsley <walmsleymk1@gmail.com>
2025-07-09 13:24:01 -0400

    and from imagenet replication

--- gz_evo/downstream/slurm_examples/finetune_all_galahad.sh
+++ gz_evo/downstream/slurm_examples/finetune_all_galahad.sh
@@ -67,12 +67,12 @@ echo HF_TOKEN_PATH $HF_TOKEN_PATH
 # so400m_patch14 = shape optimized ('getting vits in shape'), 400M variant, 14x14 patches
 # vit_base_patch16 = 16x16 patches, 86M parameters (from original paper, also the smallest!)
 LEARNER="vit_so400m_siglip"
-# ENCODER_HUB_PATH="hf_hub:timm/vit_so400m_patch14_siglip_gap_224.v2_webli"  # <- this one
+ENCODER_HUB_PATH="hf_hub:timm/vit_so400m_patch14_siglip_gap_224.v2_webli"  # <- this one for imagenet standard
 # ENCODER_HUB_PATH="hf_hub:timm/vit_so400m_patch14_siglip_224.v2_webli" 
 # ENCODER_HUB_PATH="hf_hub:timm/vit_base_patch16_siglip_224.v2_webli" 
 # ENCODER_HUB_PATH="hf_hub:timm/vit_base_patch16_siglip_gap_224.v2_webli" 
 # and my new finetuned version
-ENCODER_HUB_PATH="hf_hub:mwalmsley/baseline-encoder-regression-vit_so400m_siglip_ft"
+# ENCODER_HUB_PATH="hf_hub:mwalmsley/baseline-encoder-regression-vit_so400m_siglip_ft"
 
 # LEARNER='maxvit_base'
 # ENCODER_HUB_PATH="hf_hub:mwalmsley/baseline-encoder-regression-maxvit_base"
