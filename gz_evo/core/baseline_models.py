run at full precision, seems to fix nans

gz_evo/core/baseline_models.py
@@ -20,7 +20,7 @@ class GenericBaseline(L.LightningModule):
         # encoder args
         architecture_name: str = 'convnext_nano',
         timm_kwargs = {},
-        channels: float = 3,
+        channels: int = 3,
         # training/finetuning args
         learning_rate: float = 1e-4,
         weight_decay: float = 0.05,
@@ -40,6 +40,12 @@ class GenericBaseline(L.LightningModule):
     
         self.encoder = timm.create_model(architecture_name, in_chans=channels, num_classes=0, **timm_kwargs)
         logging.debug(f'Encoder created: {self.encoder}')
+
+        # test with dummy input
+        # dummy_input = torch.randn(1, channels, 224, 224)
+        # x_sanity = self.encoder(dummy_input)
+        # assert not torch.isnan(x_sanity).any(), "Encoder output contains NaNs before training, on sanity check"
+
         self.head = self.create_head()
 
         self.learning_rate = learning_rate
@@ -71,7 +77,6 @@ class GenericBaseline(L.LightningModule):
 
     # copied from Zoobot - plausible that finetuning for core datasets will be important
     def configure_optimizers(self):  
-        
 
         logging.info(f'Encoder architecture to finetune: {type(self.encoder)}')
         if hasattr(self.encoder, 'vit'):  # e.g. mae
@@ -91,17 +96,27 @@ class GenericBaseline(L.LightningModule):
             weight_decay=self.weight_decay,
             layer_decay= self.layer_decay
         )
+
         # add head parameters to optimizer
         optimizer.add_param_group({'params': self.head.parameters(), 'lr': self.learning_rate})
+
+        # for debugging - simplified version
+        # optimizer = torch.optim.SGD(params=self.head.parameters(), lr=1e-4, weight_decay=0.)
+
 
         logging.info("Optimizer ready")
 
         return optimizer
         
     def forward(self, x):
+        #assert no nans
+        # assert not torch.isnan(x).any(), "Input tensor contains NaNs"
         assert x.shape[1] < 4  # torchlike BCHW
         x = self.encoder(x)
-        return self.head(x)
+        # assert not torch.isnan(x).any(), "Encoded tensor contains NaNs"
+        x = self.head(x)
+        # assert not torch.isnan(x).any(), "Head prediction tensor contains NaNs"
+        return x
     
     def make_step(self, batch, step_name):
         x = batch['image']

gz_evo/core/baseline_training.py
@@ -92,7 +92,8 @@ def get_config(architecture_name, dataset_name, save_dir, debug=False):
             nodes=1,
             # epochs=3,
             epochs=1000,
-            precision="16-mixed",  # bf16 doesn't support lgamma for dirichlet loss
+            # precision="16-mixed",  # bf16 doesn't support lgamma for dirichlet loss
+            precision="32-true",
             plugins=None,
             patience=5,
             grad_clip_val=10.0, # much more generous
@@ -106,7 +107,7 @@ def get_config(architecture_name, dataset_name, save_dir, debug=False):
         cfg.batch_size = 8
         cfg.accumulate_grad_batches = 2
         cfg.epochs = 20
-        cfg.overfit_batches = 2
+        cfg.overfit_batches = 5
     else:
         cfg.batch_size = cfg[cfg.batch_size_key]
         # always the same effective batch size, after accumulation
@@ -168,8 +169,8 @@ def run_training(cfg, lightning_model, datamodule):
 
     # log a few images to make sure the transforms look good
     # only do on main process
-    # if os.environ.get('SLURM_PROCID', '0') == '0':  # slurm env var
-        # log_images(wandb_logger, datamodule)
+    if os.environ.get('SLURM_PROCID', '0') == '0':  # slurm env var
+        log_images(wandb_logger, datamodule)
 
     monitor_metric = 'validation/supervised_loss' 
     checkpoint_callback = ModelCheckpoint(
@@ -258,6 +259,10 @@ def log_images(wandb_logger, datamodule):
     # print([x.shape for x in np_images])
     # print([(x.min(), x.max()) for x in np_images])
     # exit()
+
+    # check no nans
+    assert not np.isnan(np_images).any(), "NaN values found in images"
+
     wandb_logger.experiment.log(
         {
             "train_examples": [