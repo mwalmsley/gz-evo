142a316aa9f37304e01a0150d14437c661c5310a
Mike Walmsley <walmsleymk1@gmail.com>
2025-07-29 15:28:49 -0400

    vit base

--- gz_evo/core/multinomial/train.py
+++ gz_evo/core/multinomial/train.py
@@ -39,8 +39,8 @@ def main():
     # architecture_name = 'resnet50'
     # architecture_name = 'resnet50_clip.openai'
 
-    architecture_name = 'vit_small_patch16_224.dino' 
-    # architecture_name = 'vit_base_patch16_clip_224.openai'  # TODO first run
+    # architecture_name = 'vit_small_patch16_224.dino' 
+    architecture_name = 'vit_base_patch16_clip_224.openai'
     # architecture_name = 'vit_so400m_siglip'  # ran
 
     # architecture_name = 'beit3_base_patch16'
