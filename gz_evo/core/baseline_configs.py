f3b24e575109b8d8b580ad6eac5c36491193d5a5
Mike Walmsley <walmsleymk1@gmail.com>
2025-07-30 17:25:27 -0400

    new set of runs

--- gz_evo/core/baseline_configs.py
+++ gz_evo/core/baseline_configs.py
@@ -14,8 +14,8 @@ class ModelConfig:
     drop_path_rate: Optional[float] = 0.
     # effective batch size for training, summed over devices and after accumulation
     # target_batch_size: int = 4096
-    # target_batch_size: int = 1024  
-    target_batch_size: int = 512  
+    target_batch_size: int = 1024  
+    # target_batch_size: int = 512  
     distribution_strategy: str = "ddp_find_unused_parameters_true"  # override with ddp where possible
     scheduler_kwargs: Optional[dict] = None  # additional kwargs for the scheduler, if needed
 
@@ -393,8 +393,8 @@ CFG_EFFICIENTFORMER_V2L = ModelConfig(
     weight_decay=0.05,
     layer_decay=0.9,  # small decay
     distribution_strategy='ddp',
-    # learning_rate=1e-4,  # this worked very well
-    learning_rate=1e-3,  # increased
+    learning_rate=1e-4,  # this worked very well with batch 4k
+    # learning_rate=1e-3,  # increased
     # scheduler_kwargs={'sched':'cosine', 'warmup_lr':1e-5, 'warmup_epochs':5, 'num_epochs':300, 'min_lr':0}
 )
 
