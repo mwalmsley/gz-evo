0b3f9f94701d81ab9faaa690d04d4ff4d59429fb
Mike Walmsley <walmsleymk1@gmail.com>
2025-08-06 15:26:02 -0400

    check rings with zoobot float

--- gz_evo/downstream/slurm_examples/finetune_all_galahad.sh
+++ gz_evo/downstream/slurm_examples/finetune_all_galahad.sh
@@ -64,7 +64,7 @@ echo HF_TOKEN_PATH $HF_TOKEN_PATH
 # ENCODER_HUB_PATH="hf_hub:mwalmsley/baseline-encoder-regression-convnext_nano"
 # ENCODER_HUB_PATH="hf_hub:timm/convnext_nano.in12k"  # override for imagenet timm weights
 
-LEARNER="convnext_base"
+# LEARNER="convnext_base"
 # ENCODER_HUB_PATH="hf_hub:mwalmsley/baseline-encoder-regression-convnext_base"
 
 # LEARNER="maxvit_tiny_rw_224"  # only regression, not evo prepared
@@ -151,7 +151,7 @@ DIVISOR=1
 
 # LEARNER="convnextv2_base"
 
-# LEARNER="efficientformerv2_l"
+LEARNER="efficientformerv2_l"
 
 # LEARNER="vit_so400m_siglip"
 
